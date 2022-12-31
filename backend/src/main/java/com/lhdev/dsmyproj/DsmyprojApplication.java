d7f71a2d486c15726749bff90e5f82bc15dae874
LR Holanda <luiz.holanda1408@gmail.com>
2022-12-31 17:22:14 +0100

    Project Createdcommit

---
+++ backend/src/main/java/com/lhdev/dsmyproj/DsmyprojApplication.java
@@ -0,0 +1,13 @@
+package com.lhdev.dsmyproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmyprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmyprojApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lhdev/dsmyproj/DsmyprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhdev.dsmyproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmyprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
