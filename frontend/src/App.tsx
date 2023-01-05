5aefe2cd0597cd9825000259b262f901b41720f3
LR Holanda <luiz.holanda1408@gmail.com>
2023-01-05 19:56:36 +0100

    First Component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
+
+function App() {
+  return (
+    <>
+      <h1>Olá</h1>
+      <NotificationButton/>
+    </>
+  )
+}
+
+export default App;
