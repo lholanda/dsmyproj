61a6adcd753876bc337e1f56f65ccd6592eb76ac
LR Holanda <luiz.holanda1408@gmail.com>
2023-01-06 21:09:52 +0100

    useState

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -6,10 +6,10 @@ function Header() {
     <header>
       <div className="dsmeta-logo-container">
         <img src={logo} alt="DSMeta" />
-        <h1>DSMeta</h1>
+        <h1>DSMyProj</h1>
         <p>
           Desenvolvido por
-          <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+          <a href="https://www.lhdev.com/"> Luiz Holanda</a>
         </p>
       </div>
     </header>
